e67d7297157e4c66b3e7071513ce8ee72c8f279e
Alex Sifuentes <aleximpact18@gmail.com>
2026-06-03 17:21:11 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# About This Portfolio
+<p> This Portfolio contains early beginning HTML/CSS/JS attempts mixed in with advanced usage later on throughout my academic journey.
---
+++ script.js
@@ -0,0 +1,39 @@
+
+//Lasat Updated: 7/8/2024
+
+//darkmode
+
+function toggleDarkMode() {
+    const body = document.body;
+    const darkModeButton = document.getElementById('dark-mode-button');
+
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        darkModeButton.innerHTML = '😄 Light Mode';
+    } else {
+        darkModeButton.innerHTML = '😴 Dark Mode';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-theme');
+        document.getElementById('dark-mode-button').innerHTML = '😄 Light Mode';
+    }
+});
+
+//navbar
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.style.top = '80px';
+    const navbarNav = navbar.querySelector('.navbar-nav'); 
+    navbarNav.style.justifyContent = "center";
+
+    const navLinks = navbarNav.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+    link.style.padding = '0.5rem 4rem 0.5rem 2rem';
+    link.style.fontWeight = 'bold';
+    link.style.fontfamily = 'Arial, sans-serif';
+});
+});
